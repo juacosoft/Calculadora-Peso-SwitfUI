a374712ba1fe9077e45aedb7e357c42c32402b03
juacosoftdev <joaquinalfonso.martinez@gmail.com>
2024-02-19 15:54:17 -0500

    Initial Commit

---
+++ CalculadoraPesoSUI/CalculadoraPesoSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraPesoSUIApp.swift
+//  CalculadoraPesoSUI
+//
+//  Created by Joaquin Martinez on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraPesoSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
