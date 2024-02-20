ad19d814380eda5eed6348a76013ab8b8b2cc81e
juacosoftdev <joaquinalfonso.martinez@gmail.com>
2024-02-20 10:24:01 -0500

    Calculadora

--- CalculadoraPesoSUI/ContentView.swift
+++ CalculadoraPesoSUI/ContentView.swift
@@ -8,12 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var indiceGrasa: String = "_____"
+    @State var porcentajeGrasaTxt:String = "%0.0"
+    @State var peso: String = ""
+    @State var altura: String = ""
+    
+    func calcularIndice(peso: Float, altura: Float) {
+        let porcentaje = peso / altura
+        porcentajeGrasaTxt = "%\(porcentaje)"
+        if porcentaje > 25 {
+            indiceGrasa = "Alto"
+        } else {
+            indiceGrasa = "Normal"
+        }
+    }
+    
+    func resetAll(){
+        indiceGrasa = "_____"
+        porcentajeGrasaTxt = "%0.0"
+        peso = ""
+        altura = ""
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack (alignment: .center, spacing: 12
+        ) {
+            Text("Porcentaje de grasa:").font(.body)
+            Text(porcentajeGrasaTxt).font(.largeTitle)
+            Text("Tu índice de grasa es:").font(.body)
+            Text(indiceGrasa).font(.largeTitle)
+            
+            HStack(alignment: .center) {
+                Text("Peso: ").font(.body)
+                TextField("0", text: $peso).border(.black)
+                Text("Altura: ").font(.body)
+                TextField("0", text: $altura).border(.black)
+            }.padding()
+            Spacer()
+            Button(action: {
+                if let pesoFloat = Float(peso), let alturaFloat = Float(altura) {
+                    calcularIndice(peso: pesoFloat, altura: alturaFloat)
+                }
+            }){
+                Text("Calcular")
+            }.buttonStyle(CalculatingButtonStyle())
+            Button(action: {
+                resetAll()
+            }){
+                Text("Limpiar")
+            }.buttonStyle(ClearingButtonStyle())
+            Spacer()
         }
         .padding()
     }
@@ -22,5 +67,33 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// Estilo del botón Calcular
+struct CalculatingButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(10)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+    }
+}
+
+// Estilo del botón Limpiar
+struct ClearingButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(Color.primary)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
 }
